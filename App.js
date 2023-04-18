a3cab804e7fcede49328a41fb43c930a1abb8b6d
Johnathan Simeroth <john.simeroth@gmail.com>
2023-04-17 21:44:41 -0700

    added home components and moved styles to a lib file

--- App.js
+++ App.js
@@ -1,47 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import { Text, View, SafeAreaView, FlatList } from 'react-native';
 import axios from 'axios';
 import Home from './components/Home.jsx';
+import styles from './lib/styles.js';
+
 
 export default function App() {
-  // const [events, setEvents] = useState([]);
-  // useEffect(() => { getAndSetEvents() }, []);
-
-  // const getAndSetEvents = () => {
-  //   axios.get('http://localhost:8000/reservations')
-  //     .then((response) => { setEvents(response.data) })
-  //     .catch((err) => console.error(err));
-  // };
 
   return (
     <SafeAreaView style={styles.safe}>
-      <Text>Top Level App</Text>
+      <Text style={styles.text}>Top Level App</Text>
       <Home />
-      {/* <Nav /> */}
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  safe: {
-    flex: 1,
-    backgroundColor: '#000',
-  },
-});
-
-{/* <FlatList
-        data={events}
-        renderItem={({item}) => (
-          <View >
-              <Text >{item.summary}</Text>
-              <Text >{item.start.date} - {item.end.date}</Text>
-          </View>
-        )}
-        keyExtractor={item => item.id}
-      /> */}
--- components/Home.jsx
+++ components/Home.jsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, SafeAreaView, FlatList } from 'react-native';
 import Dashboard from './home/Dashboard.jsx';
 import TasksList from './home/TasksList.jsx';
+import styles from '../lib/styles.js';
 
 export default function Home() {
 
   return (
     <View >
-      <Text >Home screen</Text>
-
+      <Text style={styles.text}>Home screen</Text>
+      <Dashboard />
+      <TasksList />
     </View>
   );
 }
